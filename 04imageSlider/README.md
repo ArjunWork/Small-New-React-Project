fc9fe3bfbfdcbae266638c09f4d109cd0d659d56
ArjunWork <arjunworkinbox@gmail.com>
2024-02-05 00:27:53 +0530

    uploading final files

--- 04imageSlider/README.md
+++ 04imageSlider/README.md
@@ -1,8 +1,17 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Project : Image Slider React Component 
+<br>
+This is a React component that renders an image slider using the react-icons library for navigation arrows and provides basic error handling for graceful user experience. It supports pagination for displaying large image sets.
+<br><br>
+Features :
+Displays a sequence of images with smooth transitions.
+Provides navigation controls (arrows or dots) to move between images.
+Supports fetching images from an external API with pagination for large datasets.
+Offers basic error handling to gracefully manage API request failures.
+<br> <br>
+Technologies Used :
+<br>
+React - JavaScript library for building user interfaces.
+<br>
+Tailwind CSS - Utility-first CSS framework for rapid development.
+<br>
+react-icons: Library for convenient access to common icons.
--- 04imageSlider/src/components/ImageSlider.jsx
+++ 04imageSlider/src/components/ImageSlider.jsx
@@ -1,18 +1,19 @@
-import React from "react";
 import { useEffect, useState } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import "./style.css";
 
-function ImageSlider({ url, limit, page }) {
+export default function ImageSlider({ url, limit = 5, page = 1 }) {
   const [images, setImages] = useState([]);
-  const [currentSlider, setCurrentSlider] = useState(0);
-  const [loading, setLoading] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   async function fetchImages(getUrl) {
     try {
       setLoading(true);
-      const resposne = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
-      const data = await resposne.json();
+
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
 
       if (data) {
         setImages(data);
@@ -24,50 +25,69 @@ function ImageSlider({ url, limit, page }) {
     }
   }
 
-  useEffect(() => {
-    if (url !== "") {
-      fetchImages(url);
-    }
-  }, [url]);
-
-  if (loading) {
-    {
-      return (
-        <div className="w-full text-center bg-red-100 font-bold">
-          Loading Data ! Please Wait
-        </div>
-      );
-    }
+  function handlePrevious() {
+    setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1);
   }
 
-  if (images) {
-    console.log(images);
+  function handleNext() {
+    setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
+  }
+
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url]);
+
+  console.log(images);
+
+  if (loading) {
+    return <div>Loading data ! Please wait</div>;
   }
 
   if (errorMsg !== null) {
-    return (
-      <div className="w-full text-center bg-red-100 font-bold">
-        Error occured ! {errorMsg}
-      </div>
-    );
+    return <div>Error occured ! {errorMsg}</div>;
   }
 
   return (
-    <div className="w-full min-h-[100vh] bg-gray-500">
-        <BsArrowLeftCircleFill className=""/>
-        {images && images.length 
-        ?  images.map((imageItem)=>{
-            <img
-            key={imageItem.id}
-            alt = {imageItem.download_url}
-            src = {imageItem.download_url}
-            className="" 
-            />
-        })
-        :  null}
-        <BsArrowRightCircleFill className=""/>
+    <div className="w-full min-h-[100vh] flex justify-center items-center bg-gray-500">
+      <div className="container w-full min-h-[80vh]">
+        <BsArrowLeftCircleFill
+          onClick={handlePrevious}
+          className="arrow arrow-left"
+        />
+        {images && images.length
+          ? images.map((imageItem, index) => (
+              <img
+                key={imageItem.id}
+                alt={imageItem.download_url}
+                src={imageItem.download_url}
+                className={
+                  currentSlide === index
+                    ? "current-image"
+                    : "current-image hide-current-image"
+                }
+              />
+            ))
+          : null}
+        <BsArrowRightCircleFill
+          onClick={handleNext}
+          className="arrow arrow-right"
+        />
+        <span className="circle-indicators">
+          {images && images.length
+            ? images.map((_, index) => (
+                <button
+                  key={index}
+                  className={
+                    currentSlide === index
+                      ? "current-indicator"
+                      : "current-indicator inactive-indicator"
+                  }
+                  onClick={() => setCurrentSlide(index)}
+                ></button>
+              ))
+            : null}
+        </span>
+      </div>
     </div>
-    );
+  );
 }
-
-export default ImageSlider;
